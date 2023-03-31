84e224dedc1d64072d72af5a9f99c8877f79e6bf
SergioEspadas-Ksquare <Sergio.Espadas@itksquare.edu.mx>
2023-03-30 21:36:14 -0600

    Update main.js

--- Prob_1_Analog_Clock/main.js
+++ Prob_1_Analog_Clock/main.js
@@ -2,18 +2,18 @@ const hour = document.querySelector(".hour");
 const minutes = document.querySelector(".minutes");
 const seconds = document.querySelector(".seconds")
 
-function actualTime() {
+function actualTime() {//get the information about the hour
     time = new Date();
 
     hr = time.getHours();
     min = time.getMinutes();
     sec = time.getSeconds();
 
-    hrHand = 30 * hr + min/2;
+    hrHand = 30 * hr + min/2;//calculate the movement
     minHand = 6 * min;
     secHand = 6 * sec;
 
-    hour.style.transform = `rotate(${hrHand}deg)`;
+    hour.style.transform = `rotate(${hrHand}deg)`;//adding rotation to the hands
     minutes.style.transform = `rotate(${minHand}deg)`;
     seconds.style.transform = `rotate(${secHand}deg)`;
 }
